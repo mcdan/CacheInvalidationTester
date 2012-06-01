294429b38aa9eaabaad3b59e5b3b25005ef0dcae
dan mcweeney <mcweeney@adobe.com>
2012-06-01 16:15:58 -0400

    Conform even more to HTTP standard ( i hope )

--- server.py
+++ server.py
@@ -4,19 +4,19 @@ from SimpleHTTPServer import SimpleHTTPRequestHandler
 import time
 from datetime import timedelta
 class SimpleHandler(SimpleHTTPRequestHandler, object):
-	lastModified = ""
+	lastModified = time.time()
 	def do_GET(self):
-		if SimpleHandler.lastModified == "":
-			SimpleHandler.lastModified = self.date_time_string(time.time())
 		if "data.json" in self.path:
 			json = "{\"one\": 1, \"two\": 2, \"three\": 3}"
 			self.send_response(200)
 			self.send_header("Content-length", len(json))
-			self.send_header("Content-type", "application/json")
-			self.send_header("Last-Modified", SimpleHandler.lastModified)
+			self.send_header("Last-Modified", self.date_time_string(SimpleHandler.lastModified))
+			self.send_header("Expires", self.date_time_string(SimpleHandler.lastModified + 1000))
 			for header in self.headers:
 				if "x-seq" in header:
 					self.send_header(header, self.headers[header])
+			# RFC2616 14.10
+			self.send_header("Connection", "close")
 			self.end_headers()
 			self.wfile.write(json)
 		else:
@@ -24,17 +24,18 @@ class SimpleHandler(SimpleHTTPRequestHandler, object):
 	def do_POST(self):
 		resp = "{\"status\":200}"
 		self.send_response(200)
-		SimpleHandler.lastModified = self.date_time_string(time.time())
-		self.send_header("Last-Modified", SimpleHandler.lastModified)
+		SimpleHandler.lastModified = time.time()
+		self.send_header("Last-Modified", self.date_time_string(SimpleHandler.lastModified))
 		self.send_header("Content-length", len(resp))
-		self.send_header("Content-type", "application/json")
+		# RFC2616 14.10
+		self.send_header("Connection", "close")
 		self.end_headers()
 		self.wfile.write(resp)
 
 
 HandlerClass = SimpleHTTPRequestHandler
 ServerClass  = BaseHTTPServer.HTTPServer
-Protocol     = "HTTP/1.0"
+Protocol     = "HTTP/1.1"
 
 if sys.argv[1:]:
     port = int(sys.argv[1])
